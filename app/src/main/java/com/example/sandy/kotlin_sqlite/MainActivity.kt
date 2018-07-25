2d4195741510d5f62af0a1a474b7da52f0f36b02
sandeeptile <sandeeptile@gmail.com>
2018-07-25 20:25:17 +0530

    read data

--- app/src/main/java/com/example/sandy/kotlin_sqlite/MainActivity.kt
+++ app/src/main/java/com/example/sandy/kotlin_sqlite/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SimpleCursorAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -38,6 +39,47 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this,"plz try again",Toast.LENGTH_LONG).show()
 
             }
+        }
+
+        readbtn.setOnClickListener {
+
+            /*(String table, String[] columns, String selection,
+            String[] selectionArgs, String groupBy, String having,
+            String orderBy, String limit) */
+
+            /*  var c =  dBase.query("employee",null,null,
+                   null,null,null,null)*/
+
+            /*var c =  dBase.query("employee",null,
+            "emp_id=?",
+            arrayOf(et1.text.toString()),null,null,
+             null) */
+
+            /*            var c =  dBase.query("employee",null,null,
+            null,"name",null,null) */
+
+            /*         var c =  dBase.query("employee",null,null,
+              null,"emp_id","emp_id>125",
+                             null) */
+
+            /*var c =  dBase.query("employee",null,null,
+                    null,null,null,
+                    "name desc") */
+
+            var c=dBase.query("employee",null,null,null,null,null,null)
+
+            /*(Context context, int layout, Cursor c, String[] from,
+            int[] to, int flags) */
+
+            var from:Array<String> = arrayOf("emp_id",
+                    "name","email","mob_no")
+            var to: IntArray = intArrayOf(R.id.tvid,
+                    R.id.tvname,R.id.tvemail,R.id.tvmno)
+
+           var adapter=SimpleCursorAdapter(this@MainActivity,R.layout.indiview,c,from,to,0)
+
+            listview.adapter=adapter
+
         }
     }
 }
